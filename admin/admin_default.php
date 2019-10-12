0353498a5efc5c024d940176062f30ceb623a494
nurainatirah <aienatirah97@gmail.com>
2019-10-13 00:24:05 +0800

    draft admin interface untuk aliah

---
+++ admin/admin_default.php
@@ -0,0 +1,13 @@
+<?php include 'body_upper.php' ?>
+
+
+<div id="page-wrapper">
+
+ 
+
+</div>
+
+<!-- /#page-wrapper -->
+
+
+<?php include 'body_lower.php' ?>
---
+++ admin/user.php
@@ -0,0 +1,83 @@
+<?php include 'body_upper.php' ?>
+
+
+<div id="page-wrapper">
+
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Restaurant</h1>
+        </div>
+    </div>
+
+    <div class="btn-toolbar">
+        <button type="button" style="float:right" class="btn btn-primary">New Restaurant</button>
+    </div>
+
+    <br />
+
+    <table class="table table-striped table-bordered table-hover">
+
+        <thead>
+
+            <tr>
+
+                <th>No.</th>
+
+                <th>Restaurant Name</th>
+
+                <th>Data</th>
+
+                <th>Data</th>
+
+            </tr>
+
+        </thead>
+
+        <tbody>
+
+            <tr>
+
+                <td>1</td>
+
+                <td>Data</td>
+
+                <td>Data</td>
+
+                <td>Data</td>
+
+            </tr>
+
+            <tr>
+
+                <td>2</td>
+
+                <td>Data</td>
+
+                <td>Data</td>
+
+                <td>Data</td>
+
+            </tr>
+
+            <tr>
+
+                <td>3</td>
+
+                <td>Data</td>
+
+                <td>Data</td>
+
+                <td>Data</td>
+
+            </tr>
+
+        </tbody>
+
+    </table>
+
+</div>
+
+<!-- /#page-wrapper -->
+
+
+<?php include 'body_lower.php' ?>
